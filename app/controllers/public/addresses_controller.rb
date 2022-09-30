6dcb6ce707c29cdce58f0edf5bb38f04f9757b3e
EC2 Default User <ec2-user@ip-172-31-14-170.ap-northeast-1.compute.internal>
2022-09-30 10:02:52 +0000

    注文履歴詳細画面編集

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,4 +1,7 @@
 class Public::AddressesController < ApplicationController
+
+  before_action :authenticate_customer!
+
   def index
     @address = Address.new
     @addresses = current_customer.addresses
@@ -8,9 +11,27 @@ class Public::AddressesController < ApplicationController
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @address.save
-    redirect_to address_path
+    redirect_to addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
   end
 end
